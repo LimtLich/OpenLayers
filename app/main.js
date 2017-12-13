063e272ce7cc5e77daad052ce1483d6247ea01ce
limt <921136839@qq.com>
2017-12-13 17:35:11 +0800

    update

--- app/main.js
+++ app/main.js
@@ -2,19 +2,54 @@
 import Map from 'ol/map';
 import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
-import XYZ from 'ol/source/xyz';
- 
-new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+import XYZ from 'ol/source/xyz'; //加载离线
+import OSM from 'ol/source/osm' 
+import SourceVector from 'ol/source/Vector'
+import LayerVector from 'ol/layer/Vector'
+import Draw from 'ol/interaction/draw'
+
+var raster = new TileLayer({
+    source: new OSM()
 });
+
+var source = new SourceVector({
+    wrapX: false
+});
+
+var vector = new LayerVector({
+    source: source
+});
+
+var map = new Map({
+    layers: [raster, vector],
+    target: 'map',
+    view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4
+    })
+});
+
+var typeSelect = document.getElementById('type');
+
+var draw; // global so we can remove it later
+function addInteraction() {
+    var value = typeSelect.value;
+    if (value !== 'None') {
+        draw = new Draw({
+            source: source,
+            type: typeSelect.value
+        });
+        map.addInteraction(draw);
+    }
+}
+
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+    map.removeInteraction(draw);
+    addInteraction();
+};
+
+addInteraction();
