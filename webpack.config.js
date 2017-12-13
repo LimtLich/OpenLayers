0b3a057ce3a8d08c8fc7d58e38300b7ada88a424
limt <921136839@qq.com>
2017-12-13 16:13:43 +0800

    openlayers

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public", //打包后的文件存放的地方
+        filename: "bundle.js" //打包后输出文件的文件名
+    },
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: true,
+        inline: true
+    },
+    module: {
+        rules: [{
+            test: /(\.jsx|\.js)$/,
+            loader: "babel-loader",
+            exclude: /node-modules/
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader",
+                options:{
+                    modules:true
+                }
+            },{
+                loader:"postcss-loader"
+            }]
+        }]
+    }
+}
